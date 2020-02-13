5affe249ad0586db69f9d0211e27e2aae3aed575
a.morales <andresmorales@rccl.com>
2020-02-13 16:11:59 -0500

    Add first commit

---
+++ com/DronApp/Main/Main.java
@@ -0,0 +1,10 @@
+package com.DronApp.Main;
+
+import com.DroneApp.Constants.Constants;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Number of drone is " + Constants.DronNumber);
+    }
+}
---
+++ com/DroneApp/Constants/Constants.java
@@ -0,0 +1,8 @@
+package com.DroneApp.Constants;
+
+public class Constants {
+
+    public static final String path = "./resources/in.txt";
+    public static final int DronNumber = 3;
+
+}
